20af36dd1be6d554ce391fcdf8be5a58d76bfd2d
VCM admin account <vcm@vcm-24005.vm.duke.edu>
2022-01-12 15:56:37 -0500

    first attempt

---
+++ makefile
@@ -0,0 +1,2 @@
+project1: project1.c
+	gcc -o project1 -pedantic -std=gnu99 -Wall -Werror project1.c
---
+++ project1.c
@@ -0,0 +1,10 @@
+//#include "my_malloc.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+    printf("myLove\n");
+    return 1;
+}
